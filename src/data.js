f0accdd3168ad2545032f497250dc53a68aa36dc
Ankithshetty98 <109474204+Ankithshetty98@users.noreply.github.com>
2023-03-13 23:05:55 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,115 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info:  "Google Pixel is a brand of portable consumer electronic devices developed by Google that run either ChromeOS or the Android operating system. The main line of Pixel products consist of Android-powered smartphones, which have been produced since October 2016 as the replacement of the older Nexus, and of which the Pixel 7 is the current model. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 16,
+    company: "SAMSUNG",
+    info:
+      "The Galaxy S7 is an evolution of the prior year's model, with upgraded hardware, design refinements, and the restoration of features removed from the Galaxy S6, such as IP certification for water and dust resistance, as well as expandable storage with a MicroSD card. Succeeding the S6 and S6 Edge+, respectively, the S7 is produced in a standard model with a display size of 5.1-inch (130 mm) as well as an Edge variant whose display is curved along the wide sides of the screen and also has a larger 5.5-inch (140 mm) display. The S7 Active features a thicker more rugged frame and an increased battery capacity. The Galaxy S7 and S7 Edge are the last two phones in the Samsung Galaxy S series to have a physical home button with a front-sided fingerprint sensor embedded in the button.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC 10 - Black",
+    img: "img/product-3.png",
+    price: 8,
+    company: "htc",
+    info:
+      "6.6 inch, 20:9, FHD+ (1080 x 2412 pixels), Android™ 12 (Android S), Qualcomm® Snapdragon® 695 5G Mobile Platform, Extended memory: MicroSDTM (3.0, up to SDXC)",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HTC 10 - White",
+    img: "img/product-4.png",
+    price: 18,
+    company: "htc",
+    info:
+      "6.6 inch, 20:9, FHD+ (1080 x 2412 pixels), Android™ 12 (Android S), Qualcomm® Snapdragon® 695 5G Mobile Platform, Extended memory: MicroSDTM (3.0, up to SDXC)",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "HTC Desire 626s",
+    img: "img/product-5.png",
+    price: 24,
+    company: "htc",
+    info:
+      "6.6 inch, 20:9, FHD+ (1080 x 2412 pixels), Android™ 12 (Android S), Qualcomm® Snapdragon® 695 5G Mobile Platform, Extended memory: MicroSDTM (3.0, up to SDXC)",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Iphone",
+    img: "img/product-6.png",
+    price: 17,
+    company: "apple",
+    info:
+      "The iPhone 6 and 6 Plus include larger 4.7 and 5.5 inches (120 and 140 mm) displays, a faster processor, upgraded cameras, improved LTE and Wi-Fi connectivity and support for a near field communications-based mobile payments offering.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 7",
+    img: "img/product-7.png",
+    price: 30,
+    company: "apple",
+    info:
+      "The iPhone 7 features a 28mm 12-megapixel camera with optical image stabilization, a wider f/1.8 aperture 6-element lens, wide color capture, and a revamped Apple image signal processor, all of which result in brighter, sharper, more detailed photos, even in low-light conditions.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Smashed Iphone",
+    img: "img/product-8.png",
+    price: 2,
+    company: "apple",
+    info:
+      "quick Mobile service center broken display change low price service",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
